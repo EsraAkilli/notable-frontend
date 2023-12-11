a80d75d8008faf450ddb103bab1ab17488cf7881
Esra <esra@Esra-MacBook-Pro.local>
2023-12-11 17:09:41 +0300

    Show unprocessable entity errors

--- src/helpers/intercepters.js
+++ src/helpers/intercepters.js
@@ -1,4 +1,6 @@
 import axios from "axios"
+import { ToastProgrammatic as Toast } from "buefy"
+import { flattenDeep, uniq, values } from "lodash"
 
 axios.defaults.baseURL = "http://notable.test/api/"
 
@@ -13,3 +15,36 @@ axios.interceptors.request.use((config) => {
 
   return config
 })
+
+axios.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  function (error) {
+    if (error.response) {
+      if (error.response.status === 422) {
+        let errors = parseError(error.response)
+
+        Toast.open({
+          message: `<b>${error.response.data.message}</b><br>${errors}`,
+          type: "is-warning",
+          duration: 5000
+        })
+      }
+
+      return Promise.reject(error.response)
+    }
+  }
+)
+
+function parseError(e) {
+  if (!e.data.errors) {
+    return ""
+  }
+
+  if (Object.keys(e.data.errors).length > 0) {
+    return uniq(flattenDeep(values(e.data.errors))).join("<br>")
+  }
+
+  return ""
+}
